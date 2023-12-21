ffd834f3bb67a9426b45d1f3f10040911113dd81
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-21 13:25:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+let p=document.getElementById('status');
+
+let btn=document.getElementById('enterBtn');
+
+btn.addEventListener('click',fun);
+
+function fun(para) {
+	let h1=document.createElement('h1');
+	h1.id=p.id;
+	h1.innerText="Entered Metaverse"
+	p.parentNode.replaceChild(h1,p);
+}
